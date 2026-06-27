6d2a062a5d0dbc27402fd6605916c10f57b036c5
Eid-Alanazi1 <b8qhjc9j6d@privaterelay.appleid.com>
2026-06-27 20:35:25 +0300

    first commit

---
+++ ScanTeast.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (void) {
+    int a  ;
+    int b;
+    int sum;
+
+
+    printf("the first number= ");
+    scanf("%d" , &a);
+
+
+    printf("Second number = ");
+    scanf("%d" , &b);
+
+    sum = a + b ;
+printf("the sum is %d " , sum);
+return 0;
+}
---
+++ SecondClass.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+    int a = 9;
+    int b = 10 ;
+    int c = 5 ;
+    int sum = a + b - c;
+    printf("%d\n",sum);
+    return 0;
+
+}
